5d687c2f72158507bb379b503d05c0f260e9ad31
dextErr <99316883+DexterJames021@users.noreply.github.com>
2025-04-30 07:36:52 +0000

    odd or even

--- odd-even.java
+++ odd-even.java
@@ -1,10 +1,28 @@
+import java.util.*;
 class isOdd{
     public static void main(String[] args){
-        int number = 3;
-        if(number % 2 == 0){
-            System.out.println(number + "is Even");
-        }else {
-              System.out.println(number + "is Odd");
+        Scanner scan = new Scanner(System.in);
+
+        int number = scan.nextInt();
+        
+        if(number == 0){
+            System.out.println(number + " is Zero");
         }
-    }
+        else if(number < 0){
+            System.out.println(number + " is a negative number");
+        }
+        else if(number > 0){
+            System.out.println(number + " is a negative number");
+        }
+        else if(number % 2 == 0){
+            System.out.println(number + " is Even");
+
+        }
+        else {
+            System.out.println(number + " is Odd");
+
+        }
+
+        scan.close();
+} 
 } 
