e664b46e7bd402517559337dba18c6bcac74a5c0
dextErr <99316883+DexterJames021@users.noreply.github.com>
2025-04-30 07:21:51 +0000

    odd and even

---
+++ odd-even.java
@@ -0,0 +1,10 @@
+class isOdd{
+    public static void main(String[] args){
+        int number = 3;
+        if(number % 2 == 0){
+            System.out.println(number + "is Even");
+        }else {
+              System.out.println(number + "is Odd");
+        }
+    }
+} 
