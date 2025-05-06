0fc471c6a810225f7f4a5bbe4c1c100e36d78b25
dextErr <99316883+DexterJames021@users.noreply.github.com>
2025-05-06 08:34:21 +0000

    sdfsdf

--- stringBuilderPalindrome.java
+++ stringBuilderPalindrome.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class stringBuilderPalindrome {
+    // task 4
     public static void main(String[] args) {
         Scanner word = new Scanner(System.in);
         String str = word.nextLine();
