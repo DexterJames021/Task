460200b6797f2704d0306739d56b24c15dcb99d7
dextErr <99316883+DexterJames021@users.noreply.github.com>
2025-05-08 01:18:41 +0000

    update activities

---
+++ Tasks/Task113.java
@@ -0,0 +1,13 @@
+package Tasks;
+
+public class Task113 {
+
+    public static void main(String[] args) {
+        int number = 10;
+    
+        for(int i = 0; i < number; i++){
+            String message = (i % 2 == 0)? "Even": "Odd";
+            System.out.println(i + " is "+ message + " number");
+        }
+    }
+}
