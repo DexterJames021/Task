7afcbddf5ac152b785fb2404abf2386bfe4aca55
dextErr <99316883+DexterJames021@users.noreply.github.com>
2025-05-17 07:36:27 +0000

    odd even nega zero

---
+++ programmingChallenge/oddEvenNegtiveZero.java
@@ -0,0 +1,26 @@
+package programmingChallenge;
+
+import java.util.*;
+class isOdd{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        
+        if(number == 0){
+            System.out.println(number + " is Zero");
+        }
+        else if(number < 0){
+            System.out.println(number + " is a negative number");
+        }
+        else if(number % 2 == 0){
+            System.out.println(number + " is Even");
+
+        }
+        else {
+            System.out.println(number + " is Odd");
+
+        }
+
+        scan.close();
+} 
+} 
