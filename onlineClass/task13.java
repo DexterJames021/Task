355b13fab6740e2ff2b1fbb77d89a29f266fd639
dextErr <99316883+DexterJames021@users.noreply.github.com>
2025-05-24 15:46:12 +0000

    task 13

---
+++ onlineClass/task13.java
@@ -0,0 +1,37 @@
+package onlineClass;
+
+public class task13 {
+    public static void main(String[] args) {
+        Car car1 = new Car("Toyota","Camry",2022,4);
+        System.out.println(car1.displayDetails());
+    }
+}
+
+class Vehicle {
+    protected String make;
+    protected String model;
+    protected int year;
+
+    public Vehicle(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+}
+
+class Car extends Vehicle {
+    protected int numberOfDoors;
+
+    public Car(String make, String model, int year, int numberOfDoors) {
+        super(make, model, year);
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public String displayDetails() {
+        return "@@@@@ CAR DETAILS @@@@@ \n"
+                + "MAKE:" + this.make + "\n"
+                + "MODEL: " + this.model + "\n"
+                + "YEAR: " + this.year + "\n"
+                + "NUMBER OF DOORS: " + this.numberOfDoors;
+    }
+}
